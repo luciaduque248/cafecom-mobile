e3e3670dc863336589848ceb63cc8268f4ddadd5
Sara Duque <98819311+luciaduque248@users.noreply.github.com>
2026-08-28 00:44:21 -0500

    chore: remove unused SVG Metro transformer

--- metro.transformer.cjs
+++
@@ -1,9 +0,0 @@
-const upstreamTransformer = require('@expo/metro-config/babel-transformer');
-
-module.exports.transform = async ({ src, filename, options }) => {
-  if (filename.endsWith('.svg')) {
-    src = `export default ${JSON.stringify(src)};`;
-  }
-
-  return upstreamTransformer.transform({ src, filename, options });
-};
